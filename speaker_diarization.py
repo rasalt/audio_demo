15d84fa0e051e11c342bf1c4a5115733f1e08adf
Ruchika A <ruchika.kharwar@gmail.com>
2019-06-25 20:19:39 +0000

    First commit
    Signed-off-by: Ruchika A <ruchika.kharwar@gmail.com>

---
+++ speaker_diarization.py
@@ -0,0 +1,49 @@
+#Reference: https://cloud.google.com/speech-to-text/docs/async-recognize
+from google.cloud import speech_v1p1beta1 as speech
+import gcs
+#from google.cloud import storage as gcs
+
+client = speech.SpeechClient()
+
+def diarize(gcs_uri):
+
+#speech_file = 'resources/commercial_mono.wav'
+#  speech_file = gcs_uri
+  gcs_file = gcs.download_blob('diarize_demo','commercial_mono.wav')
+#  with open(speech_file, 'rb') as audio_file:
+  with open(gcs_file, 'rb') as audio_file:
+     content = audio_file.read()
+
+  audio = speech.types.RecognitionAudio(content=content)
+
+  config = speech.types.RecognitionConfig(
+      encoding=speech.enums.RecognitionConfig.AudioEncoding.LINEAR16,
+      sample_rate_hertz=8000,
+      language_code='en-US',
+      enable_speaker_diarization=True,
+      diarization_speaker_count=2)
+
+  print('Waiting for operation to complete...')
+  response = client.recognize(config, audio)
+
+# The transcript within each result is separate and sequential per result.
+# However, the words list within an alternative includes all the words
+# from all the results thus far. Thus, to get all the words with speaker
+# tags, you only have to take the words list from the last result:
+  result = response.results[-1]
+
+  words_info = result.alternatives[0].words
+
+# Printing out the output:
+  dest_file = gcs_file+".txt"
+  file = open(dest_file,"w") 
+  for word_info in words_info:
+      file.write("word: '{}', speaker_tag: {} \n".format(word_info.word,
+                                          word_info.speaker_tag))
+  file.close()
+  gcs.upload_blob('diarize_demo', dest_file)
+def main():
+        diarize('gs://diarize_demo/commercial_mono.wav')
+
+if __name__ == "__main__":
+    main()
